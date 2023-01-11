4ad63434cfb401efb5945eb746b9bdd01a0feaaa
SSneakySnek <112351775+SSneakySnek@users.noreply.github.com>
2023-01-11 13:12:25 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld
+{
+    public static void main (String[] args) {
+        
+        //Variable store data, numbers and characters
+        int a =5;
+        char b = 's';
+        long c = 500;
+        double d = 3.2;
+        
+        
+        //Methods
+        public static void addExlcamationPoint(String t) {
+
+        }
+        
+    }
+    
+}
